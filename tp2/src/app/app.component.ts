50503a680ccebb2b35671b87371498eae0a24458
FatmaSoualmia <soualmiafatma101@gmail.com>
2024-10-18 09:50:31 +0100

    rest

--- tp2/src/app/app.component.ts
+++ tp2/src/app/app.component.ts
@@ -22,4 +22,17 @@ export class AppComponent {
   //Example4
   showMe: boolean= false;
   //
+  num: number=0;
+  //
+  employees=[
+    {
+      name:"Rahul", email:"rahul@gmail.com", skills:[{skill:'Angular', exp:'2'}, {skill:'Javascript', exp:'7'}, {skill: 'TypeScript', exp:'3'}]
+    },
+    {
+      name:"Sachin", email:"sachin@gmail.com", skills:[{skill:'Angular', exp:'1'}, {skill:'Android', exp:'3'}, {skill: 'React', exp:'2'}]
+    },
+    {
+      name:"Laxmna", email:"laxman@gmail.com", skills:[{skill:'HTML', exp:'2'}, {skill:'CSS', exp:'2'}, {skill: 'Javascript', exp:'1'}]
+    }
+  ]
 }
